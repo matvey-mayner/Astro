1a9a590f5625b1adc4055d973336488d8879447c
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2024-12-14 13:49:40 +0100

    Create term.lua

---
+++ lib/term.lua
@@ -0,0 +1,24 @@
+local term = {}
+
+local component = require("component")
+local gpu = component.gpu
+
+function term.clear_screen()
+  local w, h = 80, 25
+  gpu.setResolution(resolutionX, resolutionY)
+  gpu.fill(1, 1, w, h, " ")
+end
+
+function term.print(text)
+    local x, y = 1, 1
+    for line in content:gmatch("[^\n]+") do
+        gpu.set(x, y, line)
+        y = y + 1
+        if y > resolutionY then
+            y = 1
+            term.clear()
+        end
+    end
+end
+
+return term
