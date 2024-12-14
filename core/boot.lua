97293245656a2f93480aeaf3e3f8a8349139dc5c
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2024-12-14 13:35:34 +0100

    Update boot.lua

--- core/boot.lua
+++ core/boot.lua
@@ -1,1 +1,26 @@
+local package = dofile("/lib/package.lua")
 
+local function dofile(file)
+  status("> " .. file)
+  local program, reason = raw_loadfile(file)
+  if program then
+    local result = table.pack(pcall(program))
+    if result[1] then
+      return table.unpack(result, 2, result.n)
+    else
+      error(result[2])
+    end
+  else
+    error(reason)
+  end
+end
+
+do
+  _G.component = nil
+  _G.computer = nil
+  _G.package = package
+
+  package.loaded.component = component
+  package.loaded.computer = computer
+  package.loaded.fs = dofile("/lib/fs.lua")
+end
