89bdffe651ba90ce7a7a32bbdbccfc49838ea188
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2024-12-14 13:55:10 +0100

    Update boot.lua

--- boot.lua
+++ boot.lua
@@ -12,15 +12,3 @@ do
   end
   loadfile("/core/boot.lua")(loadfile)
 end
-
-while true do
-  local result, reason = xpcall(require("shell").getShell(), function(msg)
-    return tostring(msg).."\n"..debug.traceback()
-  end)
-  if not result then
-    io.stderr:write((reason ~= nil and tostring(reason) or "unknown error") .. "\n")
-    io.write("Press any key to continue.\n")
-    os.sleep(0.5)
-    require("event").pull("key")
-  end
-end
